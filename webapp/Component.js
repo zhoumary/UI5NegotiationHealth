5aa3e8685b2222176e5aa8bd97834e18bd71c0c9
I323799 <fiona.zhou01@sap.com>
2020-01-17 16:15:23 +0800

    Get Negotiation

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,8 +1,11 @@
 sap.ui.define([
     "sap/ui/core/UIComponent",
     "sap/ui/Device",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/odata/v2/ODataModel",
     "MGitNegoHealth/model/models"
-], function(UIComponent, Device, models) {
+
+], function(UIComponent, Device, JSONModel, ODataModel, models) {
     "use strict";
 
     return UIComponent.extend("MGitNegoHealth.Component", {
@@ -24,6 +27,27 @@ sap.ui.define([
             this.setModel(models.createDeviceModel(), "device");
 
             this.getRouter().initialize();
+
+            // set Negotiation Health OData Model
+            let oModel = new ODataModel({
+                serviceUrl: "http://localhost:8081/https://my344548.crm.ondemand.com/sap/c4c/odata/cust/v1/ibso_odata_api_cn/",
+                user : "ext.fiona.zhou@vcimentos.com",
+                password : "Zyw323799",
+                serviceUrlParams : {
+                    format : "json"
+                }
+            });
+            oModel.read("/NegotiationHealthConfigCollection", {
+                success: (oData) => {
+                    let negoHealthData = oData.results;
+
+                    let allCategories = new JSONModel();
+                    allCategories.setData({modelData: negoHealthData});
+                },
+                error: (oError) => {
+
+                }
+            });
         }
     });
 });
--- webapp/controller/NegotiationHealth.controller.js
+++ webapp/controller/NegotiationHealth.controller.js
@@ -1,11 +1,61 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function(Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/odata/v2/ODataModel",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    "sap/m/MessageBox"
+], function(Controller, JSONModel, ODataModel, Filter, FilterOperator, MessageBox) {
     "use strict";
 
     return Controller.extend("MGitNegoHealth.controller.NegotiationHealth", {
-        onInit: function(oEvent){
+        onInit: function(){
+            let oView = this.getView();
+            let oViewModel = new JSONModel({
+                sul: 0,
+                se: 0,
+                cn: 0,
+                ne: 0,
+                countAll: 0,
+                formVisible: false,
+                negoData : []
+            });
+            oView.setModel(oViewModel, "negoView");
 
+            // set Negotiation Health OData Model
+            let oModel = new ODataModel({
+                serviceUrl: "http://localhost:8081/https://my344548.crm.ondemand.com/sap/c4c/odata/cust/v1/ibso_odata_api_cn/",
+                user : "ext.fiona.zhou@vcimentos.com",
+                password : "Zyw323799",
+                serviceUrlParams : {
+                    format : "json"
+                }
+            });
+            oModel.read("/NegotiationHealthConfigCollection", {
+                success: (oData) => {
+                    let negoHealthData = oData.results;
+
+                    let allCategories = new JSONModel();
+                    allCategories.setData({modelData: negoHealthData});
+                },
+                error: (oError) => {
+
+                }
+            });
+
+            let metadata = this.getOwnerComponent().getModel("negotiations");
+
+            metadata.read("/NegotiationHealthConfigCollection", {
+                success: (oData) => {
+                    let negoHealthData = oData.results;
+                    if (negoHealthData) {
+                        oView.getModel("negoView").setProperty("/negoData", negoHealthData);
+                    }
+                },
+                error: (oError) => {
+
+                }
+            });
         }
     });
 });
